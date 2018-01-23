3a904983a289681885a09246c141c9a5e76bd798
Unknown <cyborgmg@gmail.com>
2018-01-23 09:41:48 -0200

    Fim
    conclusão do curso

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Configuracao {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(Configuracao.class, args);
 	}
 
